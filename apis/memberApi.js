799d50de9e8c5d168a5ea8f90e0056a82981fc1f
cjh1939 <chemddhkd@gmail.com>
2025-04-23 17:00:45 +0900

    로그인 & 회원가입 페이지

--- apis/memberApi.js
+++ apis/memberApi.js
@@ -8,9 +8,9 @@ export const api_join = (data) => {
   return response;
 };
 
-export const api_login = (data) => {
+export const api_login = (loginData) => {
   const baseUrl =
     Platform.OS === "ios" ? "http://localhost:8080" : "http://10.0.2.2:8080";
-  const response = axios.post(`${baseUrl}/member/login`, data);
+  const response = axios.post(`${baseUrl}/users/login`, loginData);
   return response;
 };
