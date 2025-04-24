cb9b739bd14bd6b26e49264ccb1ec30d1e08a817
alee5899 <alee5899@naver.com>
2025-04-24 14:24:34 +0900

    충돌 작업

--- app/(tabs)/(home)/detail.jsx
+++ app/(tabs)/(home)/detail.jsx
@@ -1,14 +1,15 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import * as SecureStore from "expo-secure-store";
 
 const DetailScreen = () => {
   return (
     <View>
       <Text>DetailScreen</Text>
     </View>
-  )
-}
+  );
+};
 
-export default DetailScreen
+export default DetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
