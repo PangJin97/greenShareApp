f79bfdfb6f69ba2de306ef492eac046cc4a6d1b2
alee5899 <alee5899@naver.com>
2025-04-30 16:38:34 +0900

    css처리

--- app/(tabs)/community/index.jsx
+++ app/(tabs)/community/index.jsx
@@ -38,6 +38,7 @@ const ProfileHomeScreen = () => {
   //const [likeLoading, setLikeLoading] = useState({}); // 각 게시물의 좋아요 요청 상태 관리
 
 
+  // 사용자 정보 가져옴
   useFocusEffect(
     useCallback(() => {
       const fetchUserInfo = async () => {
@@ -80,10 +81,8 @@ const ProfileHomeScreen = () => {
     }, [])
   );
 
+  // 팔로우 바뀌면 화면에 반영
   const changeFollowStatus = (followId) => {
-    console.log(followId);
-    console.log(boardList[0].userEmail);
-    console.log(boardList[0].isFollow);
     
     const newBoardList = boardList.map(item => {
       if (item.userEmail === followId) {
@@ -133,8 +132,6 @@ const ProfileHomeScreen = () => {
         }}>
           <Octicons name="pencil" size={28} color="white" />
         </Pressable>
-  
-      {/* <Toast ref={(ref) => Toast.setRef(ref)} /> */}
     </View>
   );
   
