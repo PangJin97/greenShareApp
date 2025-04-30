e952d2be3b82422daec36e4111ec4250146e619e
alee5899 <alee5899@naver.com>
2025-04-30 12:05:48 +0900

    팔로우 기능 완성

--- app/(tabs)/community/index.jsx
+++ app/(tabs)/community/index.jsx
@@ -83,6 +83,24 @@ const ProfileHomeScreen = () => {
     }, [])
   );
 
+  const changeFollowStatus = (followId) => {
+    console.log(followId);
+    console.log(boardList[0].userEmail);
+    console.log(boardList[0].isFollow);
+    
+    const newBoardList = boardList.map(item => {
+      if (item.userEmail === followId) {
+        return {
+          ...item,
+          isFollow: item.isFollow === 'Y' ? 'N' : 'Y'
+        };
+      }
+      return item;
+    });
+
+    setBoardList([...newBoardList]);
+  }
+
 
   return (
     <View style={styles.container}>
@@ -103,7 +121,7 @@ const ProfileHomeScreen = () => {
               });
             }}
           >
-            <CommunityItem item={item} />
+            <CommunityItem item={item} changeFollowStatus={changeFollowStatus}/>
           </Pressable>
         )}
         keyExtractor={(item) => item.boardNum.toString()}
